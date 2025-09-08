009d374fe19ae458c0bc6de81ef844c252e402ef
Setht124 <setht124@gmail.com>
2025-09-08 11:38:57 -0700

    started making character ship

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from constants import *
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black", rect=None, special_flags=0)
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
+
 
 
 
